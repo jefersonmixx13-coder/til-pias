c135a1a9266245b2700ceb4d34d5b75abcb2832c
jefersonmixx13-coder <jefersonmixx13@gmail.com>
2025-08-22 18:08:45 -0300

    Substituir imagem no componente Story

--- src/components/Story.tsx
+++ src/components/Story.tsx
@@ -25,7 +25,7 @@ const Story = () => {
         <div className="bg-white rounded-2xl shadow-xl p-8 md:p-12">
           <div className="mb-8">
             <img
-              src="https://images.pexels.com/photos/13796954/pexels-photo-13796954.jpeg"
+              src="https://i.imgur.com/i3h8dc4.jpeg"
               alt="Tanques de criação de tilápias - exemplo de sucesso"
               className="w-full h-64 object-cover rounded-lg shadow-lg"
             />
